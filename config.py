110096063be7c40a0d483af1987284b484f7bc90
glrh11 <glrh11@163.com>
2016-05-18 00:19:39 +0800

    basic file structure

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'a n string haha aha'
+	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+	FLASK_MAIL_SUBJECT_PREFIX = '[glrh11]'
+	FLASK_MAIL_SENDER = 'glrh11 <glrh11@163.com>'
+	FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
+
+	@staticmethod
+	def init_app(app):
+		pass
+
+	class DevelopmentConfig(Config):
+		DEBUG = True
+		MAIL_SERVER = 'smtp.mail.163.com'
+		MAIL_PORT = 587
+		MAIL_USE_TLS = True
+		MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+		MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+		SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev/sqlite')
+
+	class TestingConfig(Config):
+		TESTING = True
+		SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+	class ProductionConfig(Config):
+		SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+	config = {
+	'development': DevelopmentConfig,
+	'testing': TestingConfig,
+	'production': ProductionConfig,
+
+	'default': DevelopmentConfig
+	}
---
+++ manage.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+import os
+from app import create_app, db
+from app.models import User, Role
+from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrageCommand
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+def make_shell_context():
+	return dict(app=app, db=db, User=User, Role=Role)
+
+maneger.add_command("shell", Shell(make_context=make_shell_context))
+manage.add_command('db', MigrageCommand)
+
+if __name__ == '__main__':
+	manager.run()
