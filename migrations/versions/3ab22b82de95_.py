debd1ee7a2885b60b5ad45f6e9aa99235335f100
wang li <glrh11@163.com>
2016-07-05 21:39:53 +0800

    change logo

---
+++ migrations/versions/3ab22b82de95_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 3ab22b82de95
+Revises: df7a0645cd60
+Create Date: 2016-06-28 23:00:26.943459
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3ab22b82de95'
+down_revision = 'df7a0645cd60'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
